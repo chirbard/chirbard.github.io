4f59ed338c9e0c867ec884981c272ccec8fdce4f
chirbard <matamm@outlook.com>
2024-04-11 09:29:01 +0300

    change to vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faReact } from '@fortawesome/free-brands-svg-icons'
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen flex justify-center items-center flex-col">
+      <div className='flex flex-row'>
+        <img src="https://media.giphy.com/media/hvRJCLFzcasrR4ia7z/giphy.gif" width='30'/>
+        <h1 className='text-3xl font-medium'>Hi</h1>
+      </div>
+      <h2 className="text-xl font-medium">
+      This site uses Vite + <FontAwesomeIcon icon={faReact} /> React + Typescript + Tailwind CSS 3
+      </h2>
     </div>
   );
 }
