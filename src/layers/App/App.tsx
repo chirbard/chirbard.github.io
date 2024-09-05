5ba9a7108b618dfedc6832e08db342c84c50b064
chirbard <matamm@outlook.com>
2024-09-05 19:06:50 +0300

    refactor: remove comments

--- src/layers/App/App.tsx
+++ src/layers/App/App.tsx
@@ -45,28 +45,11 @@ import LogoButton from "../../site/components/LogoButton";
 export default function App() {
   return (
     <div>
-      {/* <h1>Basic Example</h1>
-
-      <p>
-        This example demonstrates some of the core features of React Router
-        including nested <code>&lt;Route&gt;</code>s,{" "}
-        <code>&lt;Outlet&gt;</code>s, <code>&lt;Link&gt;</code>s, and using a
-        "*" route (aka "splat route") to render a "not found" page when someone
-        visits an unrecognized URL.
-      </p> */}
-
-      {/* Routes nest inside one another. Nested route paths build upon
-            parent route paths, and nested route elements render inside
-            parent route elements. See the note about <Outlet> below. */}
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="clock" element={<Clock />} />
           <Route path="calculator" element={<Calculator />} />
-
-          {/* Using path="*"" means "match anything", so this route
-                acts like a catch-all for URLs that we don't have explicit
-                routes for. */}
           <Route path="*" element={<NoMatch />} />
         </Route>
       </Routes>
@@ -77,41 +60,11 @@ export default function App() {
 function Layout() {
   return (
     <div>
-      {/* A "layout route" is a good place to put markup you want to
-          share across all the pages on your site, like navigation. */}
-      {/* <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/dashboard">Dashboard</Link>
-          </li>
-          <li>
-            <Link to="/nothing-here">Nothing Here</Link>
-          </li>
-          <li>
-            <Link to="/clock">Clock</Link>
-          </li>
-          <li>
-            <Link to="/calculator">Calculator</Link>
-          </li>
-        </ul>
-      </nav> */}
       <header className="h-24 flex justify-between items-center px-8">
         <p className="font-semibold w-28">BY MARKUS TAMM</p>
         <LogoButton />
         <p className="font-semibold w-28 text-right">2024</p>
       </header>
-
-      {/* <hr /> */}
-
-      {/* An <Outlet> renders whatever child route is currently active,
-          so you can think about this <Outlet> as a placeholder for
-          the child routes we defined above. */}
       <Outlet />
     </div>
   );
